5159c75a58c95b5de79ceb31172101bf9574473e
M1croZavr <tlashukov@gmail.com>
2024-12-22 19:34:55 +0300

    Config updates

--- src/fastapi_service/s3/__init__.py
+++ src/fastapi_service/s3/__init__.py
@@ -8,10 +8,10 @@ load_dotenv()  # Load environment variables from .env file if exists
 ACCESS_KEY = os.getenv("MINIO_ROOT_USER")
 SECRET_KEY = os.getenv("MINIO_ROOT_PASSWORD")
 
-minio_host = "minio"
-minio_port = os.getenv("MINIO_PORT", 9000)
+MINIO_HOST = "minio"
+MINIO_PORT = os.getenv("MINIO_PORT", 9000)
 client = Minio(
-    f"{minio_host}:{minio_port}",
+    f"{MINIO_HOST}:{MINIO_PORT}",
     access_key=ACCESS_KEY,
     secret_key=SECRET_KEY,
     secure=False,
--- src/models/train.py
+++ src/models/train.py
@@ -1,7 +1,7 @@
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import MLFlowLogger
 
-from fastapi_service.config import MLFLOW_PORT
+from fastapi_service.config import MLFLOW_PORT, MLFLOW_HOST
 from fastapi_service.s3.utils import save_checkpoint
 from fastapi_service.utils import get_checkpoint_dir_and_name
 
@@ -25,7 +25,7 @@ def train(
     mlf_logger = MLFlowLogger(
         experiment_name="LightningModels",
         run_name=f"{dataset_folder_name}/{model_filename}",
-        tracking_uri=f"http://mlflow:{MLFLOW_PORT}",
+        tracking_uri=f"http://{MLFLOW_HOST}:{MLFLOW_PORT}",
         log_model=True,
     )
     mlf_logger.log_hyperparams({"epochs": epochs})
